8f4e5eeef34e6a7f33164ce432e493b1dc2cf8ad
Matteo Collina <hello@matteocollina.com>
2015-07-09 09:21:38 +0200

    Added color support.

--- bench.js
+++ bench.js
@@ -1,20 +1,44 @@
 'use strict'
 
 var fastseries = require('fastseries')
+var chalk = require('chalk')
+var colors = Object.keys(chalk.styles.colors)
+var console = require('console')
 
-function build (functions, max) {
+// avoid black
+colors.shift()
 
+function build (functions, opts) {
+
+  var max
   var series = fastseries()
+  var currentColor = 0
+
+  if (typeof opts === 'object') {
+    max = opts.max || opts.iterations
+  } else {
+    max = opts
+  }
+
+  if (!max) {
+    throw new Error('missing number of iterations')
+  }
 
   return run
 
   function run (done) {
+    currentColor = 0
     series(null, bench, functions, done || noop)
   }
 
   function bench (func, done) {
     var key = func.name + '*' + max
     var count = -1
+
+    // true by default
+    if (opts.color !== false) {
+      key = chalk[nextColor()](key)
+    }
 
     console.time(key)
     end()
@@ -29,6 +53,13 @@ function build (functions, max) {
         }
       }
     }
+  }
+
+  function nextColor () {
+    if (currentColor === colors.length) {
+      currentColor = 0
+    }
+    return colors[currentColor++]
   }
 }
 
--- test.js
+++ test.js
@@ -2,6 +2,8 @@
 
 var test = require('tape')
 var bench = require('./')
+var proxyquire = require('proxyquire')
+var chalk = require('chalk')
 
 test('calls multiple functions', function (t) {
   t.plan(42 * 2)
@@ -30,4 +32,92 @@ test('supports a callback', function (t) {
   ], 42)
 
   run(t.pass.bind(t))
+})
+
+test('does two rounds', function (t) {
+  t.plan(42 * 2 * 2)
+
+  var run = bench([
+    function first (done) {
+      t.pass('first called')
+      setImmediate(done)
+    },
+    function second (done) {
+      t.pass('second called')
+      setImmediate(done)
+    }
+  ], 42)
+
+  run(run)
+})
+
+test('supports an option object', function (t) {
+  t.plan(42 * 2)
+
+  var run = bench([
+    function first (done) {
+      t.pass('first called')
+      setImmediate(done)
+    },
+    function second (done) {
+      t.pass('second called')
+      setImmediate(done)
+    }
+  ], { iterations: 42 })
+
+  run()
+})
+
+test('has color', function (t) {
+  var chalkEnabled = chalk.enabled
+  chalk.enabled = true
+
+  var bench = proxyquire('./', {
+    console: {
+      time: function (key) {
+        t.ok(chalk.hasColor(key), 'has color')
+      },
+      timeEnd: function (key) {
+        t.ok(chalk.hasColor(key), 'has color')
+      }
+    }
+  })
+
+  t.plan(2)
+
+  var run = bench([
+    function first (done) {
+      setImmediate(done)
+    }
+  ], { iterations: 42 })
+
+  run(function () {
+    chalk.enabled = chalkEnabled
+  })
+})
+
+test('disable color', function (t) {
+  var bench = proxyquire('./', {
+    console: {
+      time: function (key) {
+        t.notOk(chalk.hasColor(key), 'has no color')
+      },
+      timeEnd: function (key) {
+        t.notOk(chalk.hasColor(key), 'has no color')
+      }
+    }
+  })
+
+  t.plan(2)
+
+  var run = bench([
+    function first (done) {
+      setImmediate(done)
+    }
+  ], {
+    iterations: 42,
+    color: false
+  })
+
+  run()
 })
